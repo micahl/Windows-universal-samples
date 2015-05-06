0745763e179df5fccd00bc993f111f0ac94761ef
Micah Lewis <Micah.Lewis@microsoft.com>
2015-05-06 11:13:36 -0700

    Commanding sample bug fixes

--- xaml_commanding/CS/Scenario1.xaml.cs
+++ xaml_commanding/CS/Scenario1.xaml.cs
@@ -23,31 +23,9 @@ namespace Commanding
     /// </summary>
     public sealed partial class Scenario1 : Page
     {
-        private MainPage rootPage;
-
         public Scenario1()
         {
             this.InitializeComponent();
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            rootPage = MainPage.Current;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            rootPage.NotifyUser("Hello", NotifyType.StatusMessage);
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            rootPage.NotifyUser("Hello", NotifyType.ErrorMessage);
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            rootPage.NotifyUser("Hello How are you today. This is a very long message with a lot of text.\nLet's throw in some newline characters\nand\nsee\nwhat\nhappens.\nadsf\nasdf\nasdf\nasfd\nasdf\nasdf\nasfd", NotifyType.StatusMessage);
         }
     }
 }
